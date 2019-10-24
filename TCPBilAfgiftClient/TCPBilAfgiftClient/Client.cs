2989061237698279b77e2ce7f786ee4844af844a
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2019-10-24 10:44:54 +0200

    Add files via upload

---
+++ TCPBilAfgiftClient/TCPBilAfgiftClient/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TCPBilAfgiftClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            Console.WriteLine("Client ready");
+
+            Stream ns = clientSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            bool clientActive = true;
+
+            while (true)
+            {
+
+                string serverAnswer = sr.ReadLine();
+                Console.WriteLine("Server: " + serverAnswer);
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                if (message == "STOP")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Forbindelse lukket. Tast enter");
+            Console.ReadLine();
+
+            ns.Close();
+
+            clientSocket.Close();
+        }
+    }
+}
